80bfa9e95fca9f0591fca73ffa9bcb61103c9194
Lukkar90 <59205324+Lukkar90@users.noreply.github.com>
2020-03-01 22:20:50 +0100

    mathSumWidthAndHeight

--- scripts/executeScript/Ι_utils/functions/mathSumWidthAndHeight.jsx
+++ scripts/executeScript/Ι_utils/functions/mathSumWidthAndHeight.jsx
@@ -3,13 +3,20 @@ function mathSumWidthAndHeight(units, addWidth, addHeight, doc) {
     var activeDocWidth = parseInt(doc.width.toString().slice(0, -3), 10); // .slice(0, -3) cut off " px" from the string
     var activeDocHeight = parseInt(doc.height.toString().slice(0, -3), 10); // .slice(0, -3) cut off " px" from the string
 
+    var sumWidth = parseInt(addWidth, 10);  //It can't has to be number without decimals becouse it will cause bugs in resizeCanvas() function
+    var sumHeight = parseInt(addHeight, 10);
+
     if (units === "PERCENT") {
-        var sumWidth = 100 + parseInt(addWidth, 10); //It can't has to be number, without decimals becouse it will cause bugs in resizeCanvas() function
-        var sumHeight = 100 + parseInt(addHeight, 10); //It can't has to be number, without decimals becouse it will cause bugs in resizeCanvas() function
+        sumWidth += 100; 
+        sumHeight += 100;
+
     }
     else if (units === "PX") {
-        var sumWidth = activeDocWidth + parseInt(addWidth, 10); //It can't has to be number, without decimals becouse it will cause bugs in resizeCanvas() function
-        var sumHeight = activeDocHeight + parseInt(addHeight, 10); //It can't has to be number, without decimals becouse it will cause bugs in resizeCanvas() function
+        sumWidth += activeDocWidth; 
+        sumHeight += activeDocHeight; 
+        
+    } else {
+     throw new Error('Unknown unit '+ units)
     }
 
     return [ sumWidth, sumHeight ];
