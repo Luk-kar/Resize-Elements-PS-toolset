ea9a93c53246bdde38498b510cfd1d534ef76f6b
Lukkar90 <59205324+Lukkar90@users.noreply.github.com>
2020-03-01 19:37:51 +0100

    fixed blocking of accept button

--- scripts/executeScript/Ι_utils/EventHandlerBuilderMain/settingAcceptBtnBlock.jsx
+++ scripts/executeScript/Ι_utils/EventHandlerBuilderMain/settingAcceptBtnBlock.jsx
@@ -7,7 +7,7 @@ EventHandlerBuilderMain.prototype.settingAcceptBtnBlock = function() {
 
         if ((UI.groupWidth.numb.text.match(/[0-9]+$/) !== null) && (UI.groupHeight.numb.text.match(/[0-9]+$/) !== null) &&
             ((parseInt(UI.groupWidth.numb.text, 10) !== 0) || (parseInt(UI.groupHeight.numb.text, 10) !== 0)) && //there is only one possible bug when is equasion = 0, e. g. passing value = 1-1 = 0. In worst case scenario it happens nothing.
-            ( (UI.btnRadSourceFiles.chooseFilesSourceFold === true && !isUndefined(self.sourceFilesToProcess) && (self.sourceFilesToProcess.length > 0)) ||
+            ( (UI.btnRadSourceFiles.chooseFilesSourceFold.value === true && !isUndefined(self.sourceFilesToProcess) && (self.sourceFilesToProcess.length > 0)) ||
             (UI.btnRadSourceFiles.chooseOpenedFiles.value === true) )) {
 
             UI.btnAccept.enabled = true;
