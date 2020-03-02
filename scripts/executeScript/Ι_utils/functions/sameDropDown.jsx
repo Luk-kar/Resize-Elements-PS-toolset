78eb35102521fb77c6f6527a1044bbd0192098e6
Lukkar90 <59205324+Lukkar90@users.noreply.github.com>
2020-03-02 11:12:38 +0100

    Update scripts/executeScript/Ι_utils/functions/sameDropDown.jsx
    Co-Authored-By: Szymon Łukaszczyk <szymon.lukaszczyk@gmail.com>

--- scripts/executeScript/Ι_utils/functions/sameDropDown.jsx
+++ scripts/executeScript/Ι_utils/functions/sameDropDown.jsx
@@ -1,4 +1,4 @@
-function sameDropDown(objectEvent, objectSetSameValue) {
+function setDropdownSelectionFromEvent(sourceEvent, targetDropdown) {
     if (objectEvent.selection.index !== objectSetSameValue.selection.index) {
         objectSetSameValue.selection = objectEvent.selection.index;
     }
