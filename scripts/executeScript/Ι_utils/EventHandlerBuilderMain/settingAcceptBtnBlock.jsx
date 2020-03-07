5c0bdbeb37af593df99439e0d88fdeb9dc892e4d
Lukkar90 <59205324+Lukkar90@users.noreply.github.com>
2020-03-07 12:21:08 +0100

    further refactor

--- scripts/executeScript/Ι_utils/EventHandlerBuilderMain/settingAcceptBtnBlock.jsx
+++ scripts/executeScript/Ι_utils/EventHandlerBuilderMain/settingAcceptBtnBlock.jsx
@@ -7,11 +7,9 @@ EventHandlerBuilderMain.prototype.settingAcceptBtnBlock = function() {
 
         if ((UI.groupWidth.numb.text.match(/[0-9]+$/) !== null) && (UI.groupHeight.numb.text.match(/[0-9]+$/) !== null) && 
         ((parseInt(UI.groupWidth.numb.text, 10) !== 0) || (parseInt(UI.groupHeight.numb.text, 10) !== 0)) && (
-            (UI.btnRadSourceFiles.chooseOpenedFiles.value === true) && 
-                ((UI.btnRadDestFold.same.value === true) || (UI.btnRadDestFold.other.value === true && UI.btnChooseFilesDestFold.title.text !== "Destination folder...")) ||
-            (UI.btnRadSourceFiles.chooseFilesSourceFold.value === true && !isUndefined(self.sourceFilesToProcess) && (self.sourceFilesToProcess.length > 0)) && (
-                (UI.btnRadDestFold.same.value === true) || (UI.btnRadDestFold.other.value === true && UI.btnChooseFilesDestFold.title.text !== "Destination folder...")
-            )
+            ((UI.btnRadSourceFiles.chooseOpenedFiles.value === true) || 
+            (UI.btnRadSourceFiles.chooseFilesSourceFold.value === true && !isUndefined(self.sourceFilesToProcess) && (self.sourceFilesToProcess.length > 0))) && 
+                ((UI.btnRadDestFold.same.value === true) || (UI.btnRadDestFold.other.value === true && UI.btnChooseFilesDestFold.title.text !== "Destination folder...")) 
         )) {
 
             UI.btnAccept.enabled = true;
