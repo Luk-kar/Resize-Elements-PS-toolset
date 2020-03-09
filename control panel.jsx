4be5ec268fb2933a252b9d5b207172d4cfa909ad
Lukkar90 <59205324+Lukkar90@users.noreply.github.com>
2020-03-09 15:45:53 +0100

    settings commneting

--- control panel.jsx
+++ control panel.jsx
@@ -7,7 +7,7 @@
 
 $.level = 1; // Debugging level, Level: 0 - No Break, 1 - Break, 2 - Immediate Break
 
-#include "./scripts/settings/settings - functions.jsx"; //this has to be declared here becouse some functions are used both in main and in settings
+#include "./scripts/settings/settings - functions.jsx"; // Some functions are used both in main and in settings
 
 #include "./scripts/control panel/control panel - GuiBuilderControlPanel.jsx"; // For all UIctrlPanel objects
 
