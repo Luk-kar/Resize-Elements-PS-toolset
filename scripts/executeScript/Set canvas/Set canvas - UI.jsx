67695b8c65c805a0d0e8c6f3fc053910dd124413
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-08-19 20:51:11 +0200

    UI added

--- scripts/executeScript/Set canvas/Set canvas - UI.jsx
+++ scripts/executeScript/Set canvas/Set canvas - UI.jsx
@@ -14,6 +14,36 @@ using from "../../main/main - functions.jsx":
     this.panelChangeFile.title = this.panelChangeFile.add("statictext", undefined, executeScript + ":");
     this.panelChangeFile.title.graphics.font = this.panelTitleFont;
 
+    //Group dialog selecting by size
+
+    this.groupBiggerThan = createGroupUI(this.panelChangeFile, undefined, "left", "left");
+
+    this.groupBiggerThan.title001 = this.groupBiggerThan.add("statictext", undefined, "For images bigger than:");
+    this.groupBiggerThan.title001.characters = 18;
+
+    this.groupBiggerThan.valueLowest = this.groupBiggerThan.add("edittext", undefined, "0");
+    this.groupBiggerThan.valueLowest.characters = 4;
+
+    this.groupBiggerThan.title002 = this.groupBiggerThan.add("statictext", undefined, "px");
+
+    this.groupBiggerThan.imageTooltip = this.groupBiggerThan.add("image", undefined, this.imageInfHov);
+
+    //------------------------------------------------------------------------------
+
+    this.groupLowerThan = createGroupUI(this.panelChangeFile, undefined, "left", "left");
+
+    this.groupLowerThan.title001 = this.groupLowerThan.add("statictext", undefined, "and also smaller than:");
+    this.groupLowerThan.title001.characters = 18;
+
+    this.groupLowerThan.valueHighest = this.groupLowerThan.add("edittext", undefined, this.maxResValue);
+    this.groupLowerThan.valueHighest.characters = 4;
+
+    this.groupLowerThan.title002 = this.groupLowerThan.add("statictext", undefined, "px");
+
+    this.groupLowerThan.imageTooltip = this.groupLowerThan.add("image", undefined, this.imageInfHov);
+
+    this.groupLowerThan.marginesSpaceBottom = this.panelChangeFile.add("statictext", undefined, "");
+
     //Group units value
     this.groupUnitVal = this.panelChangeFile.add("group");
 
--- scripts/executeScript/Trim canvas/Trim canvas - UI.jsx
+++ scripts/executeScript/Trim canvas/Trim canvas - UI.jsx
@@ -9,8 +9,32 @@
     this.panelChangeFile.title.graphics.font = this.panelTitleFont;
     this.panelChangeFile.add("statictext");
 
-    this.panelExplain = createPanelUI(this.panelChangeFile, undefined, "center");
-    this.panelExplain.title = this.panelExplain.add("statictext", undefined, "Now, here will be excuted new module for choosed all files");
-    this.panelExplain.title.characters = this.panelWidth + 8; //making module width the same as rest panels in window
+    //Group dialog selecting by size
 
-    this.panelChangeFile.add("statictext");
+    this.groupBiggerThan = createGroupUI(this.panelChangeFile, undefined, "left", "left");
+
+    this.groupBiggerThan.title001 = this.groupBiggerThan.add("statictext", undefined, "For images bigger than:");
+    this.groupBiggerThan.title001.characters = 18;
+
+    this.groupBiggerThan.valueLowest = this.groupBiggerThan.add("edittext", undefined, "0");
+    this.groupBiggerThan.valueLowest.characters = 4;
+
+    this.groupBiggerThan.title002 = this.groupBiggerThan.add("statictext", undefined, "px");
+
+    this.groupBiggerThan.imageTooltip = this.groupBiggerThan.add("image", undefined, this.imageInfHov);
+
+    //------------------------------------------------------------------------------
+
+    this.groupLowerThan = createGroupUI(this.panelChangeFile, undefined, "left", "left");
+
+    this.groupLowerThan.title001 = this.groupLowerThan.add("statictext", undefined, "and also smaller than:");
+    this.groupLowerThan.title001.characters = 18;
+
+    this.groupLowerThan.valueHighest = this.groupLowerThan.add("edittext", undefined, this.maxResValue);
+    this.groupLowerThan.valueHighest.characters = 4;
+
+    this.groupLowerThan.title002 = this.groupLowerThan.add("statictext", undefined, "px");
+
+    this.groupLowerThan.imageTooltip = this.groupLowerThan.add("image", undefined, this.imageInfHov);
+
+    this.groupLowerThan.marginesSpaceBottom = this.panelChangeFile.add("statictext", undefined, "");
