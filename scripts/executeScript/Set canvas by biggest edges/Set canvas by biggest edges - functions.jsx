d15d5449fec7791203edd667d1ae3bf4337820fa
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-11-01 12:57:19 +0100

    update

--- scripts/executeScript/Set canvas by biggest edges/Set canvas by biggest edges - functions.jsx
+++ scripts/executeScript/Set canvas by biggest edges/Set canvas by biggest edges - functions.jsx
@@ -26,4 +26,58 @@
 
 #include "../Ι_utils/functions/anchorSetingNew.jsx";
 
-#include "../Ι_utils/functions/preFilterFilesToProcess.jsx";
+function preFilterFilesToProcess(self, UI) {
+    
+    ExternalObject.AdobeXMPScript = new ExternalObject("lib:AdobeXMPScript");
+
+    var sourceFilesTemp = new Array;
+
+    var biggestWidth = 0;
+    var biggestHeight = 0;
+
+    for (var i = 0; i < self.sourceFolderFilesToProcess.length; i++) {
+
+        var file = new XMPFile(File(self.sourceFolderFilesToProcess[i]).fsName, XMPConst.FILE_UNKNOWN, XMPConst.OPEN_FOR_READ);
+
+        var xmp = file.getXMP();
+
+        if (xmp.doesPropertyExist(XMPConst.NS_EXIF, "PixelXDimension")) {
+
+            var currentWidth = parseInt(xmp.getProperty(XMPConst.NS_EXIF, "PixelXDimension"), 10);
+
+            var currentHeight = parseInt(xmp.getProperty(XMPConst.NS_EXIF, "PixelYDimension"), 10);
+
+            var highestValueSide = Math.max(currentWidth, currentHeight);
+
+            if (highestValueSide >= parseInt(UI.groupBiggerThan.valueLowest.text, 10) && highestValueSide <= parseInt(UI.groupLowerThan.valueHighest.text, 10)) {
+                sourceFilesTemp.push(self.sourceFolderFilesToProcess[i]);
+            }
+
+            if (biggestWidth < currentWidth) {
+                biggestWidth = currentWidth;
+            }
+            if (biggestHeight < currentHeight) {
+                biggestHeight = currentHeight;
+            }
+
+        } else { // This files does not have EXIF dimensions, so they will be checked during opening files
+
+            var activeDoc = open(File(self.sourceFolderFilesToProcess[i]));
+            var activeDocWidth = parseInt(activeDoc.width.toString().slice(0, -3), 10); // .slice(0, -3) cut off " px" from the string
+            var activeDocHeight = parseInt(activeDoc.height.toString().slice(0, -3), 10); // .slice(0, -3) cut off " px" from the string
+
+            if (biggestWidth < activeDocWidth) {
+                biggestWidth = activeDocWidth;
+            }
+            if (biggestHeight < activeDocHeight) {
+                biggestHeight = activeDocHeight;
+            }
+            activeDoc.close();
+            sourceFilesTemp.push(self.sourceFolderFilesToProcess[i]);
+        }
+    }
+
+    self.biggestWidth = biggestWidth;
+    self.biggestHeight = biggestHeight;
+    return sourceFilesTemp;
+}
